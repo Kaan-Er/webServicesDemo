05ea44f3a8f8ab35495c1d96f305a27d02379b0f
Kaan <erkaan1@outlook.com>
2020-09-02 14:43:27 +0300

    Get method v.2

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,13 @@ public class HelloWorldController {
     //URI -> /hello-world
     //method -> "Hello World"
 
-    @RequestMapping(method = RequestMethod.GET,path = "/hello-world")
+    @RequestMapping(method = RequestMethod.GET,path = "/hello-world") //2.yöntem ->> @GetMapping(path="/hello-world")
     public String helloWorld() {
         return "Hello World";
+    } //karşımıza normal string olarak çıkar.
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World Bean"); //karşımıza json objesi şeklinde çıkar.
     }
 }
